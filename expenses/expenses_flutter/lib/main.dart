a54053891b0589e2767cbb1a0d95d9e7c54d5720
Your Name <you@example.com>
2023-03-08 19:28:24 -0300

    update code diary with learning

--- expenses/expenses_flutter/lib/main.dart
+++ expenses/expenses_flutter/lib/main.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(Expenses());
+main() => runApp(ExpensesApp());
 
-class Expenses extends StatelessWidget {
+class ExpensesApp extends StatelessWidget {
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: MyHomePage()
+    );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Primeira versão'),
+
+    return Scaffold(
+      appBar:AppBar(
+        title: Text('Despesas Pessoais'),
+      ), 
+      body:  Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround, 
+        crossAxisAlignment: CrossAxisAlignment.stretch, // FLEXBOX no flutter
+
+        children:  <Widget>[
+          Container(            
+            
+            child: Card(
+            child: Text('Gráfico'),
+            elevation: 5,
+            color: Colors.blue,
+          ),
+          ),
+          Card(
+            child: Text('Lista de Transações'),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ expenses/expenses_flutter/lib/models/transaction.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/foudation.dart';
+
+class Transaction{
+
+  final String id;
+  final String title;
+  final double value;
+  final DateTime date;
+
+  Transaction({
+   requerid this.id,
+   requerid this.title,
+   requerid this.value, 
+   requerid this.date,
+
+  });
+
+
+
+
+}
